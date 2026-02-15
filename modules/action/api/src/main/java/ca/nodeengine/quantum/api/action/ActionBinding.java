8858a6afb23967900b42db05ca8c3469b0510e33
FxMorin <28154542+fxmorin@users.noreply.github.com>
2026-02-15 02:34:53 -0500

    Quantum: Add input device to digital binding

--- modules/action/api/src/main/java/ca/nodeengine/quantum/api/action/ActionBinding.java
+++ modules/action/api/src/main/java/ca/nodeengine/quantum/api/action/ActionBinding.java
@@ -1,11 +1,13 @@
 package ca.nodeengine.quantum.api.action;
 
+import ca.nodeengine.quantum.api.InputDevice;
 import ca.nodeengine.quantum.api.state.InputState;
 
 /**
  * Move away from keys to intent.
  */
 public interface ActionBinding {
+    InputDevice device();
     InputAction action();
     boolean matches(InputState state);
     float value(InputState state); // digital = 0/1, analog allowed
--- modules/action/core/src/main/java/ca/nodeengine/quantum/action/DigitalBinding.java
+++ modules/action/core/src/main/java/ca/nodeengine/quantum/action/DigitalBinding.java
@@ -1,16 +1,26 @@
 package ca.nodeengine.quantum.action;
 
+import ca.nodeengine.quantum.api.InputDevice;
 import ca.nodeengine.quantum.api.state.InputState;
+import ca.nodeengine.quantum.api.state.GlobalInputState;
 import ca.nodeengine.quantum.api.action.ActionBinding;
 import ca.nodeengine.quantum.api.action.InputAction;
+import ca.nodeengine.quantum.api.state.PerDeviceInputState;
 
 public final class DigitalBinding implements ActionBinding {
+    private final InputDevice device;
     private final InputAction action;
     private final int code;
 
-    public DigitalBinding(InputAction action, int code) {
+    public DigitalBinding(InputDevice inputDevice, InputAction action, int code) {
+        this.device = inputDevice;
         this.action = action;
         this.code = code;
+    }
+
+    @Override
+    public InputDevice device() {
+        return device;
     }
 
     @Override
@@ -20,11 +30,16 @@ public final class DigitalBinding implements ActionBinding {
 
     @Override
     public boolean matches(InputState state) {
-        return state.isDown(code);
+        if (state instanceof GlobalInputState globalState) {
+            return globalState.isKeyHeld(code) || globalState.isButtonHeld(code);
+        } else if (state instanceof PerDeviceInputState perDeviceState) {
+            return perDeviceState.isKeyHeld(device, code) || perDeviceState.isButtonHeld(device, code);
+        }
+        return false;
     }
 
     @Override
     public float value(InputState state) {
-        return state.isDown(code) ? 1.0f : 0.0f;
+        return matches(state) ? 1.0f : 0.0f;
     }
 }
