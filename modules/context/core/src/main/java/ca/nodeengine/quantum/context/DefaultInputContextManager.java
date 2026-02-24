3879552f003420a00b5d1107cbacd26cf009a7de
FxMorin <28154542+fxmorin@users.noreply.github.com>
2026-02-24 17:57:46 -0500

    QuantumInput: Remove duplicate method

--- modules/context/core/src/main/java/ca/nodeengine/quantum/context/DefaultInputContextManager.java
+++ modules/context/core/src/main/java/ca/nodeengine/quantum/context/DefaultInputContextManager.java
@@ -1,10 +1,7 @@
 package ca.nodeengine.quantum.context;
 
-import ca.nodeengine.quantum.api.action.ActionBinding;
-import ca.nodeengine.quantum.api.action.ActionMap;
 import ca.nodeengine.quantum.api.context.InputContext;
 import ca.nodeengine.quantum.api.context.InputContextManager;
-import ca.nodeengine.quantum.api.state.InputState;
 
 import java.util.*;
 
@@ -58,23 +55,6 @@ public class DefaultInputContextManager implements InputContextManager {
     @Override
     public List<InputContext> getActiveContexts() {
         return Collections.unmodifiableList(sortedActiveContexts);
-    }
-
-    @Override
-    public float getValue(InputState state, String action) {
-        List<InputContext> activeContexts = getActiveContexts();
-        for (InputContext context : activeContexts) {
-            ActionMap actionMap = context.actionMap();
-            List<ActionBinding> bindings = actionMap.getBindings(action);
-            if (!bindings.isEmpty()) {
-                float max = 0F;
-                for (ActionBinding binding : bindings) {
-                    max = Math.max(max, binding.value(state));
-                }
-                return max;
-            }
-        }
-        return 0F;
     }
 
     private void updateSortedContexts() {
