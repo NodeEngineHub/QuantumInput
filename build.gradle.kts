03f750fb286a289af637850c12791e039bfbc9e4
FxMorin <28154542+fxmorin@users.noreply.github.com>
2026-02-24 17:57:14 -0500

    NodePlugin: Switch distribution to `graalvm-community`

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,36 +4,72 @@ allprojects {
 
 plugins {
     id("NodePlugin")
-    id("com.vanniktech.maven.publish") version "0.35.0"
+    `java-library`
+    `maven-publish`
+    id("org.jreleaser") version "1.16.0"
 }
 
-mavenPublishing {
-    coordinates("$group", "intellijreactflow", "$version")
 
-    pom {
-        name.set("Intellij React Flow")
-        description.set("An intellij plugin library, which provides the ability to use React Flow within Intellij")
-        inceptionYear.set("2025")
-        url.set("https://github.com/NodeEngineHub/IntellijReactFlow")
-        licenses {
-            license {
-                name.set("GNU Lesser General Public License v3.0")
-                url.set("https://github.com/NodeEngineHub/IntellijReactFlow/blob/master/LICENSE")
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "ca.nodeengine.quantum"
+            artifactId = "quantuminput"
+            version = project.version.toString()
+
+            pom {
+                name.set("QuantumInput")
+                description.set("QuantumInput is a modular, context-aware input system for Java")
+                inceptionYear.set("2026")
+                url.set("https://github.com/NodeEngineHub/QuantumInput")
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License v3.0")
+                        url.set("https://github.com/NodeEngineHub/QuantumInput/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("fxmorin")
+                        name.set("FX Morin")
+                        url.set("https://github.com/fxmorin/")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/NodeEngineHub/QuantumInput/")
+                    connection.set("scm:git:git://github.com/NodeEngineHub/QuantumInput.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/NodeEngineHub/QuantumInput.git")
+                }
             }
-        }
-        developers {
-            developer {
-                id.set("fxmorin")
-                name.set("FX Morin")
-                url.set("https://github.com/fxmorin/")
-            }
-        }
-        scm {
-            url.set("https://github.com/NodeEngineHub/IntellijReactFlow/")
-            connection.set("scm:git:git://github.com/NodeEngineHub/IntellijReactFlow.git")
-            developerConnection.set("scm:git:ssh://git@github.com/NodeEngineHub/IntellijReactFlow.git")
         }
     }
-    publishToMavenCentral()
-    signAllPublications()
+}
+
+jreleaser {
+    signing {
+        active.set(org.jreleaser.model.Active.ALWAYS)
+        armored.set(true)
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                register("sonatype") {
+                    active.set(org.jreleaser.model.Active.ALWAYS)
+                    url.set("https://central.sonatype.com/api/v1/publisher")
+                    stagingRepository("build/staging-deploy")
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<org.jreleaser.gradle.plugin.tasks.JReleaserDeployTask>().configureEach {
+    doFirst {
+        val buildDir = project.layout.buildDirectory.get().asFile
+        val jreleaserDir = File(buildDir, "jreleaser")
+        if (!jreleaserDir.exists()) {
+            jreleaserDir.mkdirs()
+        }
+    }
 }
